604766fda2b702d674e287d54df11207cfadfbb3
Nestor Arocha <nesaro@gmail.com>
2013-11-30 15:45:38 +0000

    removed unnecessary call to tokenize

--- pydsl/Parser/Weighted.py
+++ pydsl/Parser/Weighted.py
@@ -109,8 +109,6 @@ def locate_heavier_symbol(symbols):
 class WeightedParser(TopDownParser):
     """Weighted Parser class"""
     def get_trees(self, data, showerrors = False):
-        from pydsl.Lex import lex
-        tokenized = lex(self._productionset.alphabet, data)
         result = self.__recursive_parser(self._productionset.initialsymbol, data, self._productionset.main_production, showerrors)
         finalresult = []
         for eresult in result:
